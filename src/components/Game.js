d2acb483af1f8719bf5928ace0308c43ab2b3059
Alex Eshoo <AlexEshoo@gmail.com>
2020-07-23 18:02:17 -0400

    places mines

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,7 +2,13 @@ import React from 'react';
 import MineGrid from "./Grid";
 
 function Game(props) {
-    return <MineGrid/>
+    return (
+        <MineGrid
+            rows={10}
+            cols={10}
+            mineCount={3}
+        />
+    )
 }
 
 export default Game
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Cell from "./Cell";
 import Grid from '@material-ui/core/Grid'
 
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function makeRandomGrid(rowCount, colCount, mineCount) {
+    let gridArr = [...Array(rowCount)].map(e => Array(colCount).fill(0))
+
+    let row, col, placed
+    for (let i = 0; i < mineCount; i++) {
+        placed = false
+        while (!placed) {
+            row = getRandomInt(0, rowCount)
+            col = getRandomInt(0, colCount)
+            if (!!!gridArr[row][col]) {
+                gridArr[row][col] = "mine"
+                placed = true
+            }
+        }
+    }
+
+    return gridArr
+}
+
+
 function MineGrid(props) {
+    const [mineCount, setMineCount] = useState(props.mineCount)
+
+    const arr = makeRandomGrid(2, 3, 1)
+    console.table(arr)
+
     return (
         <Grid
             container
